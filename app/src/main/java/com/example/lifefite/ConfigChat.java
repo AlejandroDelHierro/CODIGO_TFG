9112abe73b5930daf68e020d06669327dc3b985e
AlejandroDelHierro <adhd1852004@gmail.com>
2024-06-05 17:15:13 +0200

    Version final del TFG Alejandro Del Hierro.

--- app/src/main/java/com/example/lifefite/ConfigChat.java
+++ app/src/main/java/com/example/lifefite/ConfigChat.java
@@ -2,5 +2,5 @@ package com.example.lifefite;
 
 public class ConfigChat {
     //En el codigo subido a github no ssale la api key ya que no me dejaba subirla porque se puede plagiar
-    public static final String Api_Key = "";
+    public static final String Api_Key = "sk-proj-iAj0fUWGpjL0EeF58VlbT3BlbkFJOOyILLqqsxBaq1fKjsgA";
 }
